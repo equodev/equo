47c96298e50dc690c5f6db9054ef9a5ebe52a588
lonelion <lonelion@users.noreply.github.com>
2017-09-09 15:16:58 -0700

    Initial project structure and sample app
    This contains the initial project structure and a sample app with
    a Chromium browser that loads a url.

---
+++ com.make.equo/src/com/make/equo/parts/MainPagePart.java
@@ -0,0 +1,40 @@
+package com.make.equo.parts;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+
+import com.make.swtcef.Chromium;
+
+public class MainPagePart {
+
+	private Chromium browser;
+
+	@Inject
+	public MainPagePart(Composite parent) {
+		
+	}
+	
+	@PostConstruct
+    public void createControls(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(GridLayoutFactory.fillDefaults().create());
+		composite.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+
+		browser = new Chromium(composite, SWT.NONE);
+		// browser = new Chromium(parent, SWT.NONE);
+		browser.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+		setUrl("netflix.com");
+	}
+	
+	public void setUrl(String url) {
+		browser.setUrl(url);
+	}
+}
