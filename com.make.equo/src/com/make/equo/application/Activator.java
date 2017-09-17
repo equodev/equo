7dd41e9b287878cff9f13ed75cd76985ba4ed497
lonelion <lonelion@users.noreply.github.com>
2017-09-17 12:03:51 -0700

    Clean up Equo application plugin

--- com.make.equo/src/com/make/equo/application/Activator.java
+++ com.make.equo/src/com/make/equo/application/Activator.java
@@ -1,11 +1,7 @@
 package com.make.equo.application;
 
-import org.eclipse.swt.widgets.Display;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.application.ApplicationDescriptor;
-import org.osgi.service.application.ApplicationException;
 
 public class Activator implements BundleActivator {
 
@@ -13,22 +9,7 @@ public class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-//		System.out.println("bundle conteeeexttt");
 		Activator.context = context;
-//		System.out.println("bundle conteeaaaadasdaeexttt");
-//////		context.getse
-//		ServiceReference<?> serviceReference = Activator.getContext().getAllServiceReferences("org.osgi.service.application.ApplicationDescriptor", "(service.pid=com.make.equo.application)")[0];
-//		ApplicationDescriptor app = (ApplicationDescriptor) Activator.getContext().getService(serviceReference);
-////		Display.getDefault().syncExec(new Runnable() {
-////			public void run() {
-////				try {
-//					app.launch(null);
-////				} catch (ApplicationException e) {
-////					System.out.println("falla aca mal");
-////					e.printStackTrace();
-////				}
-////			}
-////		});
 	}
 
 	@Override
