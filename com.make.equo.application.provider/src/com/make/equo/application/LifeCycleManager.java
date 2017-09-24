d679ef851f838c032aa2a9f3b2f6439c607c9470
lonelion <lonelion@users.noreply.github.com>
2017-09-23 19:17:46 -0700

    Basic lifecycle manager

--- com.make.equo.application.provider/src/com/make/equo/application/LifeCycleManager.java
+++ com.make.equo.application.provider/src/com/make/equo/application/LifeCycleManager.java
@@ -1,37 +1,45 @@
-package com.make.equo.application.api;
+package com.make.equo.application;
 
-import org.eclipse.e4.core.services.events.IEventBroker;
-import org.eclipse.e4.ui.workbench.UIEvents;
-import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.workbench.lifecycle.ProcessAdditions;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import com.make.equo.application.api.ApplicationModelService;
 
 public class LifeCycleManager {
 	
-	@PostContextCreate
-    void postContextCreate(final IEventBroker eventBroker, IApplicationContext context) {
-		Display.getDefault().syncExec(new Runnable() {
-			public void run() {
-				final Shell shell = new Shell(SWT.SHELL_TRIM);
-
-		        // register for startup completed event and close the shell
-		        eventBroker.subscribe(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE,
-		                new EventHandler() {
-		                    @Override
-		                    public void handleEvent(Event event) {
-		                        shell.close();
-		                        shell.dispose();
-		                        eventBroker.unsubscribe(this);
-		                    }
-		                });
-		        // close static splash screen
-		        context.applicationRunning();
-		        shell.open();
-			}
-		});
+	@ProcessAdditions
+    void postContextCreate(IApplicationContext applicationContext, MApplication mainApplication) {
+		// ServiceReference<?> serviceReference =
+				// context.getServiceReference("com.make.equo.application.api.ApplicationLauncher");
+				// ApplicationLauncher app= (ApplicationLauncher)
+				// context.getService(serviceReference);
+		System.out.println("viene bien pasa por lifecycle");
+		BundleContext context = Activator.getContext();
+		ServiceReference<?> serviceReference = context.getServiceReference(ApplicationModelService.class.getName());
+		ApplicationModelService service = (ApplicationModelService) context.getService(serviceReference);
+		mainApplication.getChildren().add(service.getMainWindow());
+		System.out.println("viene bien terminaaa lifecycle");
+//		Display.getDefault().syncExec(new Runnable() {
+//			public void run() {
+//				final Shell shell = new Shell(SWT.SHELL_TRIM);
+//
+//		        // register for startup completed event and close the shell
+//		        eventBroker.subscribe(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE,
+//		                new EventHandler() {
+//		                    @Override
+//		                    public void handleEvent(Event event) {
+//		                        shell.close();
+//		                        shell.dispose();
+//		                        eventBroker.unsubscribe(this);
+//		                    }
+//		                });
+//		        // close static splash screen
+//		        context.applicationRunning();
+//		        shell.open();
+//			}
+//		});
     }
 }
