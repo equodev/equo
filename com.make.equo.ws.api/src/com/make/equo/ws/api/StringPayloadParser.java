b262b18da1d183bd11a8293d1c34fdee733f9e6a
lonelion <lonelion@users.noreply.github.com>
2017-12-19 11:07:34 -0800

    Return a JSON string instead of an object string

--- com.make.equo.ws.api/src/com/make/equo/ws/api/StringPayloadParser.java
+++ com.make.equo.ws.api/src/com/make/equo/ws/api/StringPayloadParser.java
@@ -1,16 +1,21 @@
 package com.make.equo.ws.api;
 
+import com.google.gson.Gson;
+
 public class StringPayloadParser implements IEquoRunnableParser<String> {
 
 	private IEquoRunnable<String> stringPayloadEquoRunnable;
+	private Gson gson;
 
 	public StringPayloadParser(IEquoRunnable<String> StringPayloadEquoRunnable) {
 		stringPayloadEquoRunnable = StringPayloadEquoRunnable;
+		this.gson = new Gson();
 	}
 
 	@Override
 	public String parsePayload(Object payload) {
-		return payload.toString();
+		String result = gson.toJson(payload);
+		return result;
 	}
 
 	@Override
