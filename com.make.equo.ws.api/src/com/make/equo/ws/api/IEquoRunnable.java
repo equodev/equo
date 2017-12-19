870ce31bed57065b4b3e7de43d22cb02fc17983d
lonelion <lonelion@users.noreply.github.com>
2017-12-19 11:07:03 -0800

    Add FunctionalInterface annotation to IEquoRunnable interface

--- com.make.equo.ws.api/src/com/make/equo/ws/api/IEquoRunnable.java
+++ com.make.equo.ws.api/src/com/make/equo/ws/api/IEquoRunnable.java
@@ -2,6 +2,7 @@ package com.make.equo.ws.api;
 
 import com.make.equo.ws.api.lambda.Newable;
 
+@FunctionalInterface
 public interface IEquoRunnable<T> extends Newable<T> {
 
 	public void run(T payload);
