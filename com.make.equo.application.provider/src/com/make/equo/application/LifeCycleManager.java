f29688f354cd72ead11b135bf71b0687d035b44a
Guillermo Zunino <guillez@gmail.com>
2018-03-03 17:57:46 -0300

    Move framework to bnd

--- com.make.equo.application.provider/src/com/make/equo/application/LifeCycleManager.java
+++ com.make.equo.application.provider/src/com/make/equo/application/LifeCycleManager.java
@@ -1,8 +1,5 @@
 package com.make.equo.application;
 
-import java.util.Optional;
-
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.lifecycle.ProcessAdditions;
 import org.eclipse.equinox.app.IApplicationContext;
@@ -14,35 +11,37 @@ import com.make.equo.application.util.FrameworkUtil;
 
 public class LifeCycleManager {
 
-	private static final String equoAppMainClass = "equoAppMainClass";
-	private static final String equoAppBundleName = "equoAppBundleName";
+//	private static final String equoAppMainClass = "equoAppMainClass";
+//	private static final String equoAppBundleName = "equoAppBundleName";
 
 	@ProcessAdditions
-	void postContextCreate(IApplicationContext applicationContext, MApplication mainApplication)
+	void postContextCreate(IApplicationContext applicationContext, MApplication mainApplication, IEquoFramework equoApp)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		String[] appArgs = (String[]) applicationContext.getArguments().get(IApplicationContext.APPLICATION_ARGS);
-		String appBundleName = getArgValue(appArgs, equoAppBundleName).get();
-		String equoAppClassName = getArgValue(appArgs, equoAppMainClass).get();
+//		String[] appArgs = (String[]) applicationContext.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+//		String appBundleName = getArgValue(appArgs, equoAppBundleName).get();
+//		String equoAppClassName = getArgValue(appArgs, equoAppMainClass).get();
 
-		Bundle equoMainAppBundle = Platform.getBundle(appBundleName);
+//		Bundle equoMainAppBundle = Platform.getBundle(appBundleName);
+		
+		Bundle equoMainAppBundle = org.osgi.framework.FrameworkUtil.getBundle(equoApp.getClass());
 
 		FrameworkUtil.INSTANCE.setMainEquoAppBundle(equoMainAppBundle);
 
-		Class<?> equoApplicationClazz = equoMainAppBundle.loadClass(equoAppClassName);
-		IEquoFramework equoApp = (IEquoFramework) equoApplicationClazz.newInstance();
+//		Class<?> equoApplicationClazz = equoMainAppBundle.loadClass(equoAppClassName);
+//		IEquoFramework equoApp = (IEquoFramework) equoApplicationClazz.newInstance();
 
 		equoApp.buildApp(new EquoApplication(new EquoApplicationModel(mainApplication)));
 	}
 
-	private Optional<String> getArgValue(String[] appArgs, String argName) {
-		if (argName == null || argName.length() == 0)
-			return Optional.empty();
-
-		for (int i = 0; i < appArgs.length; i++) {
-			if (("-" + argName).equals(appArgs[i]) && i + 1 < appArgs.length)
-				return Optional.of(appArgs[i + 1]);
-		}
-
-		return Optional.empty();
-	}
+//	protected Optional<String> getArgValue(String[] appArgs, String argName) {
+//		if (argName == null || argName.length() == 0)
+//			return Optional.empty();
+//
+//		for (int i = 0; i < appArgs.length; i++) {
+//			if (("-" + argName).equals(appArgs[i]) && i + 1 < appArgs.length)
+//				return Optional.of(appArgs[i + 1]);
+//		}
+//
+//		return Optional.empty();
+//	}
 }
---
+++ com.make.equo.testapp/src/com/make/equo/testapp/TestApp.java
@@ -0,0 +1,19 @@
+package com.make.equo.testapp;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.make.equo.application.api.IEquoFramework;
+import com.make.equo.application.model.EquoApplication;
+
+@Component
+public class TestApp implements IEquoFramework {
+
+    @Override
+    public EquoApplication buildApp(EquoApplication application) {
+        return application
+                .name("TestApp")
+                .withSingleView("http://www.equo.wemaketechnology.com")
+                .start();
+    }
+
+}
