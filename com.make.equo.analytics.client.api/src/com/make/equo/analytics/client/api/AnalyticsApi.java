2b6a64a6b6e3435321ed98fde688cb0d044323e0
lonelion <lonelion@users.noreply.github.com>
2018-04-28 17:42:40 -0700

    [#117] Create a method API for Equo Analytics for default writes

--- com.make.equo.analytics.client.api/src/com/make/equo/analytics/client/api/AnalyticsApi.java
+++ com.make.equo.analytics.client.api/src/com/make/equo/analytics/client/api/AnalyticsApi.java
@@ -3,11 +3,36 @@ package com.make.equo.analytics.client.api;
 import com.google.gson.JsonObject;
 
 public interface AnalyticsApi {
-	
+
+	/**
+	 * Register an event in which the mandatory field {<code>count</code> is 1.
+	 * 
+	 * @param eventKey
+	 */
+	public void registerEvent(String eventKey);
+
+	/**
+	 * Register an event with segmentation in which the mandatory field
+	 * {<code>count</code> is 1.
+	 * 
+	 * @param eventKey
+	 * @param segmentation
+	 */
+	public void registerEvent(String eventKey, JsonObject segmentation);
+
+	/**
+	 * Register an event with segmentation in which the mandatory field
+	 * {<code>count</code> is 1.
+	 * 
+	 * @param eventKey
+	 * @param segmentationAsString
+	 */
+	public void registerEvent(String eventKey, String segmentationAsString);
+
 	public void registerEvent(String eventKey, int count);
-	
+
 	public void registerEvent(String eventKey, int count, JsonObject segmentation);
-	
+
 	public void registerEvent(String eventKey, int count, String segmentationAsString);
-	
+
 }
--- com.make.equo.analytics.client.provider/src/com/make/equo/analytics/client/provider/AnalyticsApiImpl.java
+++ com.make.equo.analytics.client.provider/src/com/make/equo/analytics/client/provider/AnalyticsApiImpl.java
@@ -9,37 +9,53 @@ import com.make.equo.analytics.client.api.AnalyticsApi;
 import com.make.equo.analytics.internal.api.AnalyticsService;
 
 /**
- * Equo applications will call methods of this service to register custom events data.
- * For example, in our Netflix application example, let's suppose we want to know which
- * countries were top watchers of the movie The Wolf Of Wall Street.
- * The application may register a call to the Analytics API service whenever a movie is played,
- * somethings like this:
+ * Equo applications will call methods of this service to register custom events
+ * data. For example, in our Netflix application example, let's suppose we want
+ * to know which countries were top watchers of the movie The Wolf Of Wall
+ * Street. The application may register a call to the Analytics API service
+ * whenever a movie is played, somethings like this:
  * 
-{@code equoAnalytics.registerEvent("movies_summary", 1, jsonObject)}
-or
-{@code equoAnalytics.registerEvent("movies_summary", 1, "{"title": "The Wolf of Wall Street", "Country": "Germany"}")}
-
-*/
+ * {@code equoAnalytics.registerEvent("movies_summary", 1, jsonObject)} or
+ * {@code equoAnalytics.registerEvent("movies_summary", 1, "{"title": "The Wolf
+ * of Wall Street", "Country": "Germany"}")}
+ * 
+ */
 
 @Component
 public class AnalyticsApiImpl implements AnalyticsApi {
-	
+
+	private static final int DEFAULT_COUNT = 1;
 	private AnalyticsService analyticsService;
-	
+
 	public void registerEvent(String eventKey, int count) {
 		analyticsService.registerEvent(eventKey, count);
 	}
-	
+
 	public void registerEvent(String eventKey, int count, JsonObject segmentation) {
 		analyticsService.registerEvent(eventKey, count, segmentation);
 	}
-	
+
 	public void registerEvent(String eventKey, int count, String segmentationAsString) {
 		analyticsService.registerEvent(eventKey, count, segmentationAsString);
 	}
-	
-	@Reference(cardinality=ReferenceCardinality.MANDATORY)
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY)
 	void setAnalyticsService(AnalyticsService analyticsService) {
 		this.analyticsService = analyticsService;
+	}
+
+	@Override
+	public void registerEvent(String eventKey) {
+		this.registerEvent(eventKey, DEFAULT_COUNT);
+	}
+
+	@Override
+	public void registerEvent(String eventKey, JsonObject segmentation) {
+		this.registerEvent(eventKey, DEFAULT_COUNT, segmentation);
+	}
+
+	@Override
+	public void registerEvent(String eventKey, String segmentationAsString) {
+		this.registerEvent(eventKey, DEFAULT_COUNT, segmentationAsString);
 	}
 }
