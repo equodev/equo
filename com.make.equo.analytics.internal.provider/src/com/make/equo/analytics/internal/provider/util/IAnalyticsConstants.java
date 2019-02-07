c4d74ab6451d42644d82522d2d972e0427e5fcb3
Elias Menon <eliasmenon@gmail.com>
2019-02-07 20:52:55 -0300

    Changing to camel case to match logging tags

--- com.make.equo.analytics.internal.provider/src/com/make/equo/analytics/internal/provider/util/IAnalyticsConstants.java
+++ com.make.equo.analytics.internal.provider/src/com/make/equo/analytics/internal/provider/util/IAnalyticsConstants.java
@@ -3,6 +3,6 @@ package com.make.equo.analytics.internal.provider.util;
 public interface IAnalyticsConstants {
 
 	static String INFLUXDB_DATABASE_NAME = "equoapps";
-	static String APP_NAME_TAG = "app_name";
-	static String APP_VERSION_TAG = "app_version";
+	static String APP_NAME_TAG = "appName";
+	static String APP_VERSION_TAG = "appVersion";
 }
