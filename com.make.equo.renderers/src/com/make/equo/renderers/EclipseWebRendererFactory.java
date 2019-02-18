6d0a34de06044e26a65e89ebe62a253389212f48
lonelion <lonelion@users.noreply.github.com>
2019-02-18 02:13:48 -0800

    Clean up renderer factory

--- com.make.equo.renderers/src/com/make/equo/renderers/EclipseWebRendererFactory.java
+++ com.make.equo.renderers/src/com/make/equo/renderers/EclipseWebRendererFactory.java
@@ -2,33 +2,37 @@ package com.make.equo.renderers;
 
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
-import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
 import org.eclipse.e4.ui.workbench.renderers.swt.WorkbenchRendererFactory;
 
 public class EclipseWebRendererFactory extends WorkbenchRendererFactory {
 
+	private static final String EQUO_MAIN_TOOLBAR = "com.make.equo.main.toolbar";
 //	protected MPartRenderer stackRenderer;
 	protected String id;
 	private ToolBarRenderer toolBarRenderer;
-	private WebItemStackRenderer stackRenderer;
 
 	@Override
 	public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
 
-		if (uiElement instanceof MToolBar) {
+		if (isEquoMainToolBar(uiElement)) {
 			if (toolBarRenderer == null) {
 				toolBarRenderer = new ToolBarRenderer();
 				super.initRenderer(toolBarRenderer);
 			}
 			return toolBarRenderer;
-		} else if (uiElement instanceof MPartStack) {
-			if (stackRenderer == null) {
-				stackRenderer = new WebItemStackRenderer();
-				super.initRenderer(stackRenderer);
-			}
-			return stackRenderer;
 		}
+//		else if (uiElement instanceof MPartStack) {
+//			if (stackRenderer == null) {
+//				stackRenderer = new WebItemStackRenderer();
+//				super.initRenderer(stackRenderer);
+//			}
+//			return stackRenderer;
+//		}
 		return super.getRenderer(uiElement, parent);
+	}
+
+	private boolean isEquoMainToolBar(MUIElement mUIToolbar) {
+		return (mUIToolbar instanceof MToolBar) && EQUO_MAIN_TOOLBAR.equals(((MToolBar) mUIToolbar).getElementId());
 	}
 }
