cec6d1f5881d48fbfdbe0772bd0b54b19f4fd519
Elias Menon <eliasmenon@gmail.com>
2019-02-27 23:10:32 -0300

    Adding close trim to web dialog window

--- com.make.equo.renderers/src/com/make/equo/renderers/WebDialogRenderer.java
+++ com.make.equo.renderers/src/com/make/equo/renderers/WebDialogRenderer.java
@@ -34,284 +34,274 @@ import com.make.swtcef.Chromium;
 
 public class WebDialogRenderer extends WBWRenderer implements IEquoRenderer {
 
-	static final String EQUO_RENDERERS_NAME = "toolbar";
-	static final String EQUO_RENDERERS_URL = EQUO_RENDERERS_URL_PREFIX + EQUO_RENDERERS_NAME
-			+ EQUO_RENDERERS_URL_SUFFIX;
+   private static String ShellMinimizedTag = "shellMinimized"; //$NON-NLS-1$
+   private static String ShellMaximizedTag = "shellMaximized"; //$NON-NLS-1$
 
-	private static String ShellMinimizedTag = "shellMinimized"; //$NON-NLS-1$
-	private static String ShellMaximizedTag = "shellMaximized"; //$NON-NLS-1$
+   private String namespace;
+   private MWebDialog dialog;
 
-	private String namespace;
-	private MWebDialog dialog;
+   @Inject
+   private IEquoApplication equoApplication;
 
-	@Inject
-	private IEquoApplication equoApplication;
+   @Inject
+   private EquoEventHandler equoEventHandler;
 
-	@Inject
-	private EquoEventHandler equoEventHandler;
+   @Inject
+   private IEquoServer equoProxyServer;
 
-	@Inject
-	private IEquoServer equoProxyServer;
+   private Shell realParentShell;
 
-	private Shell realParentShell;
+   @Override
+   public Object createWidget(MUIElement dialog, Object parent) {
+      this.namespace = "WebDialog" + Integer.toHexString(dialog.hashCode());
+      this.dialog = (MWebDialog) dialog;
 
-	@Override
-	public Object createWidget(MUIElement dialog, Object parent) {
-		this.namespace = "WebDialog" + Integer.toHexString(dialog.hashCode());
-		this.dialog = (MWebDialog) dialog;
+      Shell parentShell = getParentShell();
 
-		Shell parentShell = getParentShell();
+      prepareShell(parentShell);
 
-		prepareShell(parentShell);
+      configureAndStartRenderProcess(realParentShell);
 
-		configureAndStartRenderProcess(realParentShell);
+      return realParentShell;
+   }
 
-		return realParentShell;
-	}
+   @Override
+   public void onActionPerformedOnElement() {
+      equoEventHandler.on(namespace + "_itemClicked", (JsonObject payload) -> {
+         JsonElement value = payload.get("command");
+         if (value != null) {
+            realParentShell.getDisplay().syncExec(() -> {
+               dialog.setResponse(value.getAsInt());
+               realParentShell.dispose();
+            });
+         }
+      });
+   }
 
-	@Override
-	public void onActionPerformedOnElement() {
-		equoEventHandler.on(namespace + "_itemClicked", (JsonObject payload) -> {
-			JsonElement value = payload.get("command");
-			if (value != null) {
-				realParentShell.getDisplay().syncExec(() -> {
-					dialog.setResponse(value.getAsInt());
-					realParentShell.dispose();
-				});
-			}
-		});
-	}
+   @Override
+   public EquoEventHandler getEquoEventHandler() {
+      return equoEventHandler;
+   }
 
-	@Override
-	public EquoEventHandler getEquoEventHandler() {
-		return equoEventHandler;
-	}
+   @Override
+   public List<Map<String, String>> getEclipse4Model() {
+      List<Map<String, String>> e4Model = new ArrayList<Map<String, String>>();
 
-	@Override
-	public List<Map<String, String>> getEclipse4Model(String namespace) {
-		List<Map<String, String>> e4Model = new ArrayList<Map<String, String>>();
+      HashMap<String, String> model = new HashMap<String, String>();
+      model.put("title", this.dialog.getTitle());
+      model.put("message", this.dialog.getMessage());
+      model.put("type", String.valueOf(this.dialog.getType()));
+      e4Model.add(model);
 
-		HashMap<String, String> model = new HashMap<String, String>();
-		model.put("title", this.dialog.getTitle());
-		model.put("message", this.dialog.getMessage());
-		model.put("type", String.valueOf(this.dialog.getType()));
-		e4Model.add(model);
+      EList<MButton> buttons = this.dialog.getButtons();
+      for (MButton e : buttons) {
+         HashMap<String, String> elementModel = new HashMap<String, String>();
+         elementModel.put("bLabel", e.getLabel());
+         elementModel.put("action", String.valueOf(e.getAction()));
+         elementModel.put("id", e.getElementId());
+         e4Model.add(elementModel);
+      }
+      return e4Model;
+   }
 
-		EList<MButton> buttons = this.dialog.getButtons();
-		for (MButton e : buttons) {
-			HashMap<String, String> elementModel = new HashMap<String, String>();
-			elementModel.put("bLabel", e.getLabel());
-			elementModel.put("action", String.valueOf(e.getAction()));
-			elementModel.put("id", e.getElementId());
-			e4Model.add(elementModel);
-		}
-		return e4Model;
-	}
+   @Override
+   public List<String> getJsFileNamesForRendering() {
+      return Lists.newArrayList("renderers/webDialogRenderer.js");
+   }
 
-	@Override
-	public List<String> getJsFileNamesForRendering() {
-		return Lists.newArrayList("renderers/webDialogRenderer.js");
-	}
+   @Override
+   public String getNamespace() {
+      return this.namespace;
+   }
 
-	@Override
-	public String getNamespace() {
-		return this.namespace;
-	}
+   @Override
+   public List<String> getFrameworkContributionJSONFileNames() {
+      return Lists.newArrayList();
+   }
 
-	@Override
-	public List<String> getFrameworkContributionJSONFileNames() {
-		return Lists.newArrayList();
-	}
+   @Override
+   public String getModelContributionPath() {
+      return "contributions/dialogs/";
+   }
 
-	@Override
-	public String getModelContributionPath() {
-		return "contributions/dialogs/";
-	}
+   @Override
+   public IEquoServer getEquoProxyServer() {
+      return equoProxyServer;
+   }
 
-	@Override
-	public IEquoServer getEquoProxyServer() {
-		return equoProxyServer;
-	}
+   @Override
+   public IEquoApplication getEquoApplication() {
+      return equoApplication;
+   }
 
-	@Override
-	public IEquoApplication getEquoApplication() {
-		return equoApplication;
-	}
+   @Override
+   public Chromium createBrowserComponent(Composite parent) {
+      Chromium browser = new Chromium(parent, SWT.NONE);
+      browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+      return browser;
+   }
 
-	@Override
-	public Chromium createBrowserComponent(Composite parent) {
-		Chromium browser = new Chromium(parent, SWT.NONE);
-		browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		return browser;
-	}
+   @Override
+   public void postProcess(MUIElement shellME) {
+      super.postProcess(shellME);
 
-	@Override
-	public void postProcess(MUIElement shellME) {
-		super.postProcess(shellME);
+      Shell shell = (Shell) shellME.getWidget();
 
-		Shell shell = (Shell) shellME.getWidget();
+      // Capture the max/min state
+      final MUIElement disposeME = shellME;
+      shell.addDisposeListener(e -> {
+         Shell shell1 = (Shell) e.widget;
+         if (disposeME != null) {
+            disposeME.getTags().remove(ShellMinimizedTag);
+            disposeME.getTags().remove(ShellMaximizedTag);
+            if (shell1.getMinimized()) {
+               disposeME.getTags().add(ShellMinimizedTag);
+            }
+            if (shell1.getMaximized()) {
+               disposeME.getTags().add(ShellMaximizedTag);
+            }
+         }
+      });
 
-		// Capture the max/min state
-		final MUIElement disposeME = shellME;
-		shell.addDisposeListener(e -> {
-			Shell shell1 = (Shell) e.widget;
-			if (disposeME != null) {
-				disposeME.getTags().remove(ShellMinimizedTag);
-				disposeME.getTags().remove(ShellMaximizedTag);
-				if (shell1.getMinimized()) {
-					disposeME.getTags().add(ShellMinimizedTag);
-				}
-				if (shell1.getMaximized()) {
-					disposeME.getTags().add(ShellMaximizedTag);
-				}
-			}
-		});
+      // Apply the correct shell state
+      if (shellME.getTags().contains(ShellMaximizedTag)) {
+         shell.setMaximized(true);
+      } else if (shellME.getTags().contains(ShellMinimizedTag)) {
+         shell.setMinimized(true);
+      }
 
-		// Apply the correct shell state
-		if (shellME.getTags().contains(ShellMaximizedTag)) {
-			shell.setMaximized(true);
-		} else if (shellME.getTags().contains(ShellMinimizedTag)) {
-			shell.setMinimized(true);
-		}
+      shell.layout(true);
+      forceLayout(shell);
+      if (shellME.isVisible()) {
+         shell.open();
 
-		shell.layout(true);
-		forceLayout(shell);
-		if (shellME.isVisible()) {
-			shell.open();
+         if (dialog.isBlocker()) {
+            runEventLoop(shell);
+         } else {
+            dialog.setResponse(IDialogConstants.OK_ID);
+         }
 
-			if (dialog.isBlocker()) {
-				runEventLoop(shell);
-			} else {
-				dialog.setResponse(IDialogConstants.OK_ID);
-			}
+      } else {
+         shell.setVisible(false);
+      }
+   }
 
-		} else {
-			shell.setVisible(false);
-		}
-	}
+   private void forceLayout(Shell shell) {
+      int i = 0;
+      while (shell.isLayoutDeferred()) {
+         shell.setLayoutDeferred(false);
+         i++;
+      }
+      while (i > 0) {
+         shell.setLayoutDeferred(true);
+         i--;
+      }
+   }
 
-	private void forceLayout(Shell shell) {
-		int i = 0;
-		while (shell.isLayoutDeferred()) {
-			shell.setLayoutDeferred(false);
-			i++;
-		}
-		while (i > 0) {
-			shell.setLayoutDeferred(true);
-			i--;
-		}
-	}
+   private void runEventLoop(Shell loopShell) {
+      Display display;
+      if (loopShell == null) {
+         display = Display.getCurrent();
+      } else {
+         display = loopShell.getDisplay();
+      }
 
-	private void runEventLoop(Shell loopShell) {
-		Display display;
-		if (loopShell == null) {
-			display = Display.getCurrent();
-		} else {
-			display = loopShell.getDisplay();
-		}
+      while (loopShell != null && !loopShell.isDisposed()) {
+         if (!display.readAndDispatch()) {
+            display.sleep();
+         }
+      }
+      if (!display.isDisposed()) {
+         display.update();
+      }
+   }
 
-		while (loopShell != null && !loopShell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
-		if (!display.isDisposed()) {
-			display.update();
-		}
-	}
+   private void prepareShell(Shell parentShell) {
+      realParentShell = new Shell(parentShell, SWT.CLOSE);
 
-	private void prepareShell(Shell parentShell) {
-		realParentShell = new Shell(parentShell, SWT.NONE);
+      realParentShell.addShellListener(new ShellAdapter() {
 
-		realParentShell.addShellListener(new ShellAdapter() {
+         @Override
+         public void shellClosed(ShellEvent event) {
+            event.doit = false; // cancel close
+            realParentShell.getDisplay().syncExec(() -> {
+               dialog.getParent().getChildren().remove(dialog);
+               realParentShell.dispose();
+            });
+         }
+      });
 
-			@Override
-			public void shellClosed(ShellEvent event) {
-				event.doit = false; // cancel close
-				realParentShell.getDisplay().syncExec(() -> {
-					dialog.getParent().getChildren().remove(dialog);
-					realParentShell.dispose();
-				});
-			}
-		});
+      realParentShell.setSize(500, 250);
 
-		realParentShell.setSize(500, 230);
+      // Center the dialog relative to the parent shell 
+      Rectangle parentBounds = parentShell.getBounds();
+      Rectangle thisBounds = realParentShell.getBounds();
+      int locationX = (parentBounds.width - thisBounds.width) / 2 + parentBounds.x;
+      int locationY = (parentBounds.height - thisBounds.height) / 2 + parentBounds.y;
+      realParentShell.setLocation(new Point(locationX, locationY));
 
-		// Center the dialog relative to the parent shell
-		Rectangle parentBounds = parentShell.getBounds();
-		Rectangle thisBounds = realParentShell.getBounds();
-		int locationX = (parentBounds.width - thisBounds.width) / 2 + parentBounds.x;
-		int locationY = (parentBounds.height - thisBounds.height) / 2 + parentBounds.y;
-		realParentShell.setLocation(new Point(locationX, locationY));
+      realParentShell.setLayout(new GridLayout(1, false));
 
-		realParentShell.setLayout(new GridLayout(1, false));
+      realParentShell.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+   }
 
-		realParentShell.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-	}
+   /**
+    * @return this dialog's parent shell
+    */
 
-	/**
-	 * @return this dialog's parent shell
-	 */
+   private Shell getParentShell() {
+      Shell parent = (Shell) this.dialog.getParentShell();
+      if (parent == null) {
+         Display d = Display.getCurrent();
 
-	private Shell getParentShell() {
-		Shell parent = (Shell) this.dialog.getParentShell();
-		if (parent == null) {
-			Display d = Display.getCurrent();
+         if (d == null) {
+            return null;
+         }
 
-			if (d == null) {
-				return null;
-			}
+         parent = d.getActiveShell();
 
-			parent = d.getActiveShell();
+         // Make sure we don't pick a parent that has a modal child (this can lock the app)
+         if (parent == null) {
+            // If this is a top-level window, then there must not be any open modal windows.
+            parent = getModalChild(Display.getCurrent().getShells());
+         } else {
+            // If we picked a parent with a modal child, use the modal child instead
+            Shell modalChild = getModalChild(parent.getShells());
+            if (modalChild != null) {
+               parent = modalChild;
+            }
+         }
+      }
+      return parent;
 
-			// Make sure we don't pick a parent that has a modal child (this can lock the
-			// app)
-			if (parent == null) {
-				// If this is a top-level window, then there must not be any open modal windows.
-				parent = getModalChild(Display.getCurrent().getShells());
-			} else {
-				// If we picked a parent with a modal child, use the modal child instead
-				Shell modalChild = getModalChild(parent.getShells());
-				if (modalChild != null) {
-					parent = modalChild;
-				}
-			}
-		}
-		return parent;
+   }
 
-	}
+   /**
+    * Returns the most specific modal child from the given list of Shells.
+    *
+    * @param toSearch
+    *                 shells to search for modal children
+    * @return the most specific modal child, or null if none
+    */
+   private static Shell getModalChild(Shell[] toSearch) {
+      int modal = SWT.APPLICATION_MODAL | SWT.SYSTEM_MODAL | SWT.PRIMARY_MODAL;
 
-	/**
-	 * Returns the most specific modal child from the given list of Shells.
-	 *
-	 * @param toSearch shells to search for modal children
-	 * @return the most specific modal child, or null if none
-	 */
-	private static Shell getModalChild(Shell[] toSearch) {
-		int modal = SWT.APPLICATION_MODAL | SWT.SYSTEM_MODAL | SWT.PRIMARY_MODAL;
+      for (int i = toSearch.length - 1; i >= 0; i--) {
+         Shell shell = toSearch[i];
 
-		for (int i = toSearch.length - 1; i >= 0; i--) {
-			Shell shell = toSearch[i];
+         // Check if this shell has a modal child
+         Shell[] children = shell.getShells();
+         Shell modalChild = getModalChild(children);
+         if (modalChild != null) {
+            return modalChild;
+         }
 
-			// Check if this shell has a modal child
-			Shell[] children = shell.getShells();
-			Shell modalChild = getModalChild(children);
-			if (modalChild != null) {
-				return modalChild;
-			}
+         // If not, check if this shell is modal itself
+         if (shell.isVisible() && (shell.getStyle() & modal) != 0) {
+            return shell;
+         }
+      }
 
-			// If not, check if this shell is modal itself
-			if (shell.isVisible() && (shell.getStyle() & modal) != 0) {
-				return shell;
-			}
-		}
-
-		return null;
-	}
-
-	@Override
-	public String getEquoRendererURL() {
-		return EQUO_RENDERERS_URL;
-	}
-
+      return null;
+   }
 }
