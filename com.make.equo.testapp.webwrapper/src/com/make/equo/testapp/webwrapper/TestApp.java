3dbd31b2e790870ecba0c3612ee1198cd4494d6b
mauriarroqui <mauriarroqui@gmail.com>
2020-01-09 15:24:16 -0300

    moved testapp to testapp.webwrapper because of naming consistency

---
+++ com.make.equo.testapp.webwrapper/src/com/make/equo/testapp/webwrapper/TestApp.java
@@ -0,0 +1,83 @@
+package com.make.equo.testapp.webwrapper;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.MessageDialogWithToggle;
+import org.osgi.service.component.annotations.Component;
+
+import com.make.equo.application.api.IEquoApplication;
+import com.make.equo.application.model.EquoApplicationBuilder;
+
+@Component
+public class TestApp implements IEquoApplication {
+
+	@Override
+	public EquoApplicationBuilder buildApp(EquoApplicationBuilder appBuilder) {
+		try {
+
+			return appBuilder.withSingleView("https://www.maketechnology.io").enableAnalytics()
+					.withCustomScript("js/testAnalytics.js").withCustomScript("js/testLogging.js").withMainMenu("File")
+					.addMenuItem("New").onClick(new Runnable() {
+
+						@Override
+						public void run() {
+							System.out.println("ON_NEW");
+
+						}
+					}).onAbout(new Runnable() {
+
+						@Override
+						public void run() {
+							System.out.println("ON_ABOUT");
+
+						}
+					}).onPreferences(new Runnable() {
+
+						@Override
+						public void run() {
+							System.out.println("ON_PREFERENCES");
+
+						}
+					}).onBeforeExit(new Runnable() {
+
+						@Override
+						public void run() {
+							System.out.println("Bye Bye Equo");
+
+						}
+					}).withMainMenu("Dialog Test").addMenuItem("Message Dialog").onClick(new Runnable() {
+
+						@Override
+						public void run() {
+							try {
+								MessageDialog.openInformation(null, "info dialog", "info msg");
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}).addMenuItem("Message Dialog with Toggle").onClick(new Runnable() {
+
+						@Override
+						public void run() {
+							try {
+								MessageDialogWithToggle.openYesNoQuestion(null, "mensaje en dialogo", "soy el mensaje",
+										"soy el toggle del mensaje", true, null, "key");
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}).start();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
