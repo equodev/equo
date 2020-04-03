301e00a045332fba4c3cf8b4b60341e1ab4f3852
Leandro <leandroperezb@gmail.com>
2020-04-03 20:09:45 -0300

    Added api to set lsp servers for extensions on Monaco editors

--- com.make.equo.eclipse.testapp/src/com/make/equo/eclipse/testapp/TestApp.java
+++ com.make.equo.eclipse.testapp/src/com/make/equo/eclipse/testapp/TestApp.java
@@ -1,15 +1,19 @@
 package com.make.equo.eclipse.testapp;
 
+import java.util.Collections;
+
 import org.osgi.service.component.annotations.Component;
 
 import com.make.equo.application.api.IEquoApplication;
 import com.make.equo.application.model.EquoApplicationBuilder;
+import com.make.equo.monaco.EquoMonacoEditor;
 
 @Component
 public class TestApp implements IEquoApplication {
 
 	@Override
 	public EquoApplicationBuilder buildApp(EquoApplicationBuilder appBuilder) {
+		EquoMonacoEditor.addLspServer("ws://127.0.0.1:3000/sampleServer", Collections.singleton("json"));
 		return null;
 	}
 
