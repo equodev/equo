cc96c3d98cd600dae75ba6975245872bdfeb3b74
Leandro <leandroperezb@gmail.com>
2020-04-20 10:27:33 -0300

    Proxy server. Working with absolute proxy path

--- com.make.equo.eclipse.testapp/src/com/make/equo/eclipse/testapp/TestApp.java
+++ com.make.equo.eclipse.testapp/src/com/make/equo/eclipse/testapp/TestApp.java
@@ -1,5 +1,8 @@
 package com.make.equo.eclipse.testapp;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.osgi.service.component.annotations.Component;
 
 import com.make.equo.application.api.IEquoApplication;
@@ -10,6 +13,11 @@ public class TestApp implements IEquoApplication {
 
 	@Override
 	public EquoApplicationBuilder buildApp(EquoApplicationBuilder appBuilder) {
+		// EquoMonacoEditor.addLspWsServer("ws://127.0.0.1:3000/sampleServer",
+		// Collections.singleton("json"));
+		EquoMonacoEditor.addLspServer(Arrays.asList("node",
+				"'/home/leandro/Descargas/jsonrpc-ws-proxy/yaml-language-server/out/server/src/server.js'", "--stdio"),
+				Collections.singleton("json"));
 		return null;
 	}
 
