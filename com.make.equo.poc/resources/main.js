a5b7a2d118fc93212f081c4b6bc389e682aaf457
make2019 <jfferreira1993@gmail.com>
2020-04-28 11:02:11 -0300

    style changes on the Web Toolbar

--- com.make.equo.poc/resources/main.js
+++ com.make.equo.poc/resources/main.js
@@ -4,11 +4,11 @@
             data: {
               title: "Equo plain App",
               toolitems: [
-              { icon: 'fas fa-search fa-2x', tooltip: 'Search', eventHandler: "SearchHandler"},
-              { icon: 'fas fa-copy fa-2x', tooltip: 'Copy', eventHandler: "CopyHandler" },
-              { icon: 'fas fa-save fa-2x', tooltip: 'Save', eventHandler: "SaveHandler"},
-              { icon: 'fas fa-play fa-2x', tooltip: 'Run',  eventHandler: "RunHandler" },
-              { icon: 'fas fa-bug fa-2x', tooltip: 'Debug', eventHandler: "DebugHandler" }
+              { icon: 'fas fa-search', tooltip: 'Search', eventHandler: "SearchHandler"},
+              { icon: 'fas fa-copy', tooltip: 'Copy', eventHandler: "CopyHandler" },
+              { icon: 'fas fa-save', tooltip: 'Save', eventHandler: "SaveHandler"},
+              { icon: 'fas fa-play', tooltip: 'Run',  eventHandler: "RunHandler" },
+              { icon: 'fas fa-bug', tooltip: 'Debug', eventHandler: "DebugHandler" }
               ]
             },
               methods: {
