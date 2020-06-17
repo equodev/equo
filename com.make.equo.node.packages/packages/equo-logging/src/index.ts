8958e3e8a34349044942d0942aec7b676216c508
Elias Menon <eliasmenon@gmail.com>
2020-06-17 02:58:29 -0300

    Initialize variables the typescript way to avoid errors showing in the
    editor

--- com.make.equo.node.packages/packages/equo-logging/src/index.ts
+++ com.make.equo.node.packages/packages/equo-logging/src/index.ts
@@ -9,23 +9,26 @@ export namespace EquoLoggingService {
 	var websocket: EquoWebSocket = EquoWebSocketService.get();
 
 	export function logInfo(message: string): void {
-		var payload: LogPayload;
-		payload.message = message;
-		payload.type = 'info';
+		var payload: LogPayload = {
+			message: message,
+			type: 'info'
+		};
 		websocket.send('loggingEvent', payload);
 	};
 
 	export function logError(message: string): void {
-		var payload: LogPayload;
-		payload.message = message;
-		payload.type = 'error';
+		var payload: LogPayload = {
+			message: message,
+			type: 'error'
+		};
 		websocket.send('loggingEvent', payload);
 	};
 
 	export function logWarn(message: string): void {
-		var payload: LogPayload;
-		payload.message = message;
-		payload.type = 'warning';
+		var payload: LogPayload = {
+			message: message,
+			type: 'warning'
+		};
 		websocket.send('loggingEvent', payload);
 	};
 }
