2a2a48255023cfe9ad34aa52eb5944f2e9029078
Elias Menon <eliasmenon@gmail.com>
2020-06-25 22:16:30 -0300

    Fix service-manager not returning correct service.

--- com.make.equo.node.packages/packages/equo-service-manager/src/index.ts
+++ com.make.equo.node.packages/packages/equo-service-manager/src/index.ts
@@ -6,22 +6,26 @@ export interface EquoService<T> {
 export namespace EquoService {
     const global = window as any;
 
-    export function get<T>(id: string, create: () => EquoService<T>): T {
+    export function get<T>(id: string, create?: () => EquoService<T>): T {
         const globalService: T = global[id] as T;
-        if (!globalService) {
+        if (!globalService && create) {
             const newService: EquoService<T> = create();
             if (!newService) {
-                throw new Error(id + ' has not been installed');
+                throw new Error(id + ' couldn\'t be created');
             }
             install<T>(newService);
+            return newService.service;
+        } else if (globalService) {
+            return globalService;
+        } else {
+            throw new Error(id + ' has not been installed');
         }
-        return globalService;
     }
 
     export function install<T>(service: EquoService<T>): void {
         if (global[service.id]) {
             return;
         }
-        global[service.id] = service;
+        global[service.id] = service.service;
     }
 }
