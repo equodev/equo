2d048a167b74a9f088202d232d3c38fcdd6aebec
make2019 <jfferreira1993@gmail.com>
2020-07-01 13:55:03 -0300

    Web Toolbar Bundle has been added. This bundle contain a Web Project with the Web Toolbar Definition:  - it was done with Vue  - it is builded as two Web Components: equo-toolitem and equo-toolbar This bundle has the same structure that Monaco Editor Bundle and PoC Bundle, so, before we work with that bundle it is necessary run the command `yarn` in bundle root directory to get dependencies necessaries to run and build the web project (using lerna js)

---
+++ com.make.equo.widgets.toolbar/project/toolbar-project/src/components/Toolitem.vue
@@ -0,0 +1,51 @@
+
+<template>
+    <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+                <v-btn class="e-btn"  v-on:click="eventHandler" icon text dark v-on="on">
+                  <font-awesome-icon :icon="['fas',icon]" />
+                </v-btn>
+            </template>
+            <span>{{tooltip}}</span>
+    </v-tooltip>
+</template>
+
+
+<script>
+import { VBtn, VTooltip } from 'vuetify/lib'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(
+  fas
+)
+
+  export default {
+    name: "equo-toolbar",
+    components: { VBtn, VTooltip, FontAwesomeIcon },
+        props:{
+        eventHandler:{
+            type: Function,
+            default: () => 1,
+        },
+        tooltip:{
+            type: String,
+            default: "Tooltip"
+        },
+        icon:{
+            type: String,
+            default: ""
+        }
+    }
+     
+  }
+</script>
+<style>
+.v-tooltip{
+  display: block;
+}
+
+@import url("./../styles/vuetify.css");
+@import url("./../styles/styles.css");
+</style>
