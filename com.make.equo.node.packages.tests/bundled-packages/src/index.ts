4769a63645f1bc407a4647aa846f17c6949f226b
Elias Menon <eliasmenon@gmail.com>
2020-07-21 17:11:09 -0300

    Localize dev-dependencies and add webpack module resolver config

--- com.make.equo.node.packages.tests/bundled-packages/src/index.ts
+++ com.make.equo.node.packages.tests/bundled-packages/src/index.ts
@@ -1,8 +1,13 @@
-import { EquoFramework } from '../../../com.make.equo.node.packages/packages/equo-framework/lib/index';
-import { EquoLoggingService } from '../../../com.make.equo.node.packages/packages/equo-logging/lib/index';
-import { EquoAnalyticsService } from '../../../com.make.equo.node.packages/packages/equo-analytics/lib/index';
-import { EquoMonaco } from '../../../com.make.equo.node.packages/packages/equo-monaco-editor/lib/index';
-import { EquoWebSocketService, EquoWebSocket } from '../../../com.make.equo.node.packages/packages/equo-websocket/lib/index';
+// @ts-ignore
+import { EquoFramework } from '@equo/framework';
+// @ts-ignore
+import { EquoLoggingService } from '@equo/logging';
+// @ts-ignore
+import { EquoAnalyticsService } from '@equo/analytics';
+// @ts-ignore
+import { EquoMonaco } from '@equo/equo-monaco-editor';
+// @ts-ignore
+import { EquoWebSocketService, EquoWebSocket } from '@equo/websocket';
 
 var websocket: EquoWebSocket = EquoWebSocketService.get();
 
