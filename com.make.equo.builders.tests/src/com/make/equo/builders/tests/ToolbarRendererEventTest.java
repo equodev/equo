def1d539ab9ec0da23458bd3f1558bc9727e3e5c
Elias Menon <eliasmenon@gmail.com>
2020-07-30 01:59:56 -0300

    Formatting

--- com.make.equo.builders.tests/src/com/make/equo/builders/tests/ToolbarRendererEventTest.java
+++ com.make.equo.builders.tests/src/com/make/equo/builders/tests/ToolbarRendererEventTest.java
@@ -22,63 +22,62 @@ import com.make.equo.renderers.ToolBarRenderer;
 import com.make.equo.ws.api.IEquoWebSocketService;
 
 public class ToolbarRendererEventTest extends AbstractBuilderTest {
-	
-	
-	
+
 	@Inject
 	private IEquoWebSocketService messageService;
 
 	@Before
 	public void settingContext() {
-		this.injector = injector.withApplicationContext(modelTestingConfigurator.getMainApplication(), new EclipseWebRendererFactory());
+		this.injector = injector.withApplicationContext(modelTestingConfigurator.getMainApplication(),
+				new EclipseWebRendererFactory());
 	}
-	
+
 	private Boolean executed;
-	
+
 	@Test
 	public void toolbarRenderer_Should_Store_ClickEventHandler_in_WSocket_EventsHash() throws Exception {
-		
+
 		executed = false;
 		String tooltip = "text1";
-		
-		ToolbarItemBuilder toolItemBuilder = appBuilder.plainApp("/").withToolbar().addToolItem("text", tooltip).onClick(() -> 
-		executed = true);
-				
-		//obtaining Item Model via reflection
+
+		ToolbarItemBuilder toolItemBuilder = appBuilder.plainApp("/").withToolbar().addToolItem("text", tooltip)
+				.onClick(() -> executed = true);
+
+		// obtaining Item Model via reflection
 		ToolbarBuilder toolbarBuilder = toolItemBuilder.getToolbarBuilder();
 		MToolBar toolbar = (MToolBar) getValueFromField(ToolbarBuilder.class, toolbarBuilder, "toolbar");
-		MHandledItem toolItem = (MHandledItem) getValueFromField(toolItemBuilder.getClass().getSuperclass(), toolItemBuilder, "item");
-		
-		
+		MHandledItem toolItem = (MHandledItem) getValueFromField(toolItemBuilder.getClass().getSuperclass(),
+				toolItemBuilder, "item");
+
 		createToolbarRenderer(toolbar);
 		initializeCommandAndHandlerAddons();
-		
-		
-		assertThat(messageService).isNotNull()
-			.extracting("eventHandlers").asInstanceOf(InstanceOfAssertFactories.map(String.class, String.class)).isNotNull()
-			.containsKey("toolbar" + toHexString(toolbar.hashCode())+ "_itemclicked");
-		
+
+		assertThat(messageService).isNotNull().extracting("eventHandlers")
+				.asInstanceOf(InstanceOfAssertFactories.map(String.class, String.class)).isNotNull()
+				.containsKey("toolbar" + toHexString(toolbar.hashCode()) + "_itemclicked");
+
 		// Testing invoke action from WebSocketClient
 		EmptyClient.create(new URI("ws://localhost:" + messageService.getPort()))
-			.singleMessage("{\"action\":\""+"Toolbar" + toHexString(toolbar.hashCode())+ "_itemClicked"
-					+ "\",\"params\":{\"toolBarElementId\":\""+toolItem.getElementId()+"\",\"commandId\":\""+ toolItem.getCommand().getElementId()+"\",\"isAnEquoModelElement\":\"true\"}}")
-			.connect();	
-		
+				.singleMessage("{\"action\":\"" + "Toolbar" + toHexString(toolbar.hashCode()) + "_itemClicked"
+						+ "\",\"params\":{\"toolBarElementId\":\"" + toolItem.getElementId() + "\",\"commandId\":\""
+						+ toolItem.getCommand().getElementId() + "\",\"isAnEquoModelElement\":\"true\"}}")
+				.connect();
+
 		await().untilAsserted(() -> assertThat(executed).isTrue());
 
 	}
 
 	private void initializeCommandAndHandlerAddons() {
-		this.injector.getCommandAddon().init(); 	
-		this.injector.getHandlerAddon().postConstruct(modelTestingConfigurator.getMainApplication(), this.injector.getModelService());
+		this.injector.getCommandAddon().init();
+		this.injector.getHandlerAddon().postConstruct(modelTestingConfigurator.getMainApplication(),
+				this.injector.getModelService());
 	}
 
-	private void createToolbarRenderer(MToolBar toolbar) {	
-		ToolBarRenderer renderer = (ToolBarRenderer) injector.getRendererFactory().getRenderer(toolbar, null); 
+	private void createToolbarRenderer(MToolBar toolbar) {
+		ToolBarRenderer renderer = (ToolBarRenderer) injector.getRendererFactory().getRenderer(toolbar, null);
 
 		renderer.setToolbarContext(toolbar);
 		renderer.onActionPerformedOnElement();
 	}
-	
 
 }
