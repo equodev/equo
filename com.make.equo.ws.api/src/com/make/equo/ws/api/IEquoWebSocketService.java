997acd4cc35126a5cad9e05b1c4084655ea51e28
Leandro <leandroperezb@gmail.com>
2021-01-26 09:55:17 -0300

    Solve some warnings

--- com.make.equo.ws.api/src/com/make/equo/ws/api/IEquoWebSocketService.java
+++ com.make.equo.ws.api/src/com/make/equo/ws/api/IEquoWebSocketService.java
@@ -1,7 +1,5 @@
 package com.make.equo.ws.api;
 
-import com.make.equo.ws.api.actions.IActionHandler;
-
 public interface IEquoWebSocketService {
 
 	public void addEventHandler(String actionId, IEquoRunnableParser<?> equoRunnableParser);
