77347dab3a1aea244645153e93e09dbe8f98dd16
Leandro <leandroperezb@gmail.com>
2021-03-17 18:47:54 -0300

    Exit dialog

--- com.make.equo.poc/src/com/make/equo/poc/eventhandlers/ExitAppHandler.java
+++ com.make.equo.poc/src/com/make/equo/poc/eventhandlers/ExitAppHandler.java
@@ -1,5 +1,7 @@
 package com.make.equo.poc.eventhandlers;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.osgi.service.component.annotations.Component;
 
 import com.make.equo.poc.payloads.Payload;
@@ -11,9 +13,13 @@ public class ExitAppHandler implements IActionHandler<Payload> {
 
 	@Override
 	public Object call(Payload payload) {
-		System.exit(0);
+		MessageDialog dialog = new MessageDialog(null, "Exit", null,
+				"Are you sure you want to exit?", MessageDialog.QUESTION, 1,
+				new String[] { "&Exit", "Cancel" });
+		if (dialog.open() == IDialogConstants.OK_ID) {
+			System.exit(0);
+		}
 		return null;
 	}
 
-	
 }
