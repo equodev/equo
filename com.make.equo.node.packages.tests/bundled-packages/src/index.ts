1384b959fd8dda5379cf87f592ed2c95ee49cdb6
Leandro <leandroperezb@gmail.com>
2021-03-22 12:24:56 -0300

    Removed non-core packages

--- com.make.equo.node.packages.tests/bundled-packages/src/index.ts
+++ com.make.equo.node.packages.tests/bundled-packages/src/index.ts
@@ -1,28 +1,11 @@
 // @ts-ignore
 import { EquoFramework } from '@equo/framework';
 // @ts-ignore
-import { EquoLoggingService } from '@equo/logging';
-// @ts-ignore
-import { EquoAnalyticsService } from '@equo/analytics';
-// @ts-ignore
 import { EquoWebSocketService, EquoWebSocket } from '@equo/websocket';
 // @ts-ignore
 import { Menu, MenuBuilder } from '@equo/equo-application-menu';
 
 var websocket: EquoWebSocket = EquoWebSocketService.get();
-
-websocket.on("_makeLogs", () => {
-    EquoLoggingService.logInfo('testInfo');
-    EquoLoggingService.logWarn('testWarn');
-    EquoLoggingService.logError('testError');
-});
-
-EquoAnalyticsService.registerEvent({
-    key: 'testEvent',
-    segmentation: {
-        testKey: 'testValue'
-    }
-});
 
 EquoFramework.openBrowser('');
 
