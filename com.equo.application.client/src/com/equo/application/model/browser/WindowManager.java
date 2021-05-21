5cc09196514075c0c84e246d284bd71d88d109e7
Leandro <leandroperezb@gmail.com>
2021-05-21 17:20:04 -0300

    Added static method to access the WindowManager API

--- com.equo.application.client/src/com/equo/application/model/browser/WindowManager.java
+++ com.equo.application.client/src/com/equo/application/model/browser/WindowManager.java
@@ -1,5 +1,8 @@
 package com.equo.application.model.browser;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -18,5 +21,22 @@ public class WindowManager {
 
 	public void updateBrowser(BrowserParams browserParams) {
 		eventHandler.send("updateBrowser", new Gson().toJsonTree(browserParams).getAsJsonObject());
+	}
+
+	/**
+	 * 
+	 * @return a WindowManager instance or null if there was an error getting the component
+	 */
+	public static WindowManager getInstance() {
+		BundleContext ctx = FrameworkUtil.getBundle(WindowManager.class).getBundleContext();
+		if (ctx != null) {
+			@SuppressWarnings("unchecked")
+			ServiceReference<WindowManager> serviceReference = (ServiceReference<WindowManager>) ctx
+					.getServiceReference(WindowManager.class.getName());
+			if (serviceReference != null) {
+				return ctx.getServiceObjects(serviceReference).getService();
+			}
+		}
+		return null;
 	}
 }
