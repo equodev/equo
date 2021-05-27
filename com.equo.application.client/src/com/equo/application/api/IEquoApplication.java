e183db5f123397a09e32b0337498df26e6f66822
Leandro <leandroperezb@gmail.com>
2021-05-27 16:52:23 -0300

    Remove methods from IEquoApplication

--- com.equo.application.client/src/com/equo/application/api/IEquoApplication.java
+++ com.equo.application.client/src/com/equo/application/api/IEquoApplication.java
@@ -1,31 +1,9 @@
 package com.equo.application.api;
 
-import com.google.gson.JsonObject;
-import com.equo.application.handlers.ParameterizedCommandRunnable;
 import com.equo.application.model.EquoApplicationBuilder;
-import com.equo.application.model.EquoApplicationModel;
-import com.equo.application.util.IConstants;
 
 public interface IEquoApplication {
 
 	public EquoApplicationBuilder buildApp(EquoApplicationBuilder appBuilder);
-	public static void openBrowser(String url, String name, String position) {
-		JsonObject params = new JsonObject();
-		params.addProperty("name", name);
-		params.addProperty("url", url);
-		params.addProperty("position", position);
-		
-		new ParameterizedCommandRunnable(IConstants.EQUO_WEBSOCKET_OPEN_BROWSER,
-				EquoApplicationModel.getApplicaton().getMainApplication().getContext()).run(params.toString());
-	};
-	
-	public static void updateBrowser(String url, String name) {
-		JsonObject params = new JsonObject();
-		params.addProperty("name", name);
-		params.addProperty("url", url);
-		
-		new ParameterizedCommandRunnable(IConstants.EQUO_WEBSOCKET_UPDATE_BROWSER,
-				EquoApplicationModel.getApplicaton().getMainApplication().getContext()).run(params.toString());
-	};
 
 }
