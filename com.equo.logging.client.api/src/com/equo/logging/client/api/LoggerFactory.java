f4f5bdca76805703ea1893bf3def0994210d28cd
Leandro <leandroperezb@gmail.com>
2021-06-29 21:04:54 -0300

    Lazy implementations

--- com.equo.logging.client.api/src/com/equo/logging/client/api/LoggerFactory.java
+++ com.equo.logging.client.api/src/com/equo/logging/client/api/LoggerFactory.java
@@ -22,6 +22,8 @@
 
 package com.equo.logging.client.api;
 
+import java.util.Optional;
+
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -37,7 +39,11 @@ public class LoggerFactory {
   private static boolean loaded = false;
 
   static <T extends Object> T getService(final Class<T> clazz) {
-    final BundleContext bundleContext = FrameworkUtil.getBundle(clazz).getBundleContext();
+    Bundle ctxBundle = FrameworkUtil.getBundle(clazz);
+    if (ctxBundle == null) {
+      return null;
+    }
+    final BundleContext bundleContext = ctxBundle.getBundleContext();
     if (!loaded) {
       String bundleName =
           System.getProperty(IConstants.LOGGER_BUNDLE_PROPERTY, IConstants.DEFAULT_LOGGER);
@@ -66,9 +72,161 @@ public class LoggerFactory {
   @SuppressWarnings("rawtypes")
   public static Logger getLogger(final Class clazz) {
     Logger logger = getService(Logger.class);
+    if (logger == null) {
+      return new LazyLogger(clazz);
+    }
     if (logger instanceof AbstractLogger) {
       ((AbstractLogger) logger).init(clazz);
     }
     return logger;
+  }
+
+  @SuppressWarnings("rawtypes")
+  private static class LazyLogger implements Logger {
+    private Logger instance;
+    private Class clazz;
+
+    LazyLogger(Class clazz) {
+      this.clazz = clazz;
+    }
+
+    private void getInstance() {
+      if (instance == null) {
+        instance = LoggerFactory.getLogger(clazz);
+      }
+    }
+
+    @Override
+    public void debug(String message) {
+      getInstance();
+      instance.debug(message);
+    }
+
+    @Override
+    public void debug(String message, Object... args) {
+      getInstance();
+      instance.debug(message, args);
+    }
+
+    @Override
+    public void debug(String message, Throwable throwable) {
+      getInstance();
+      instance.debug(message, throwable);
+    }
+
+    @Override
+    public void info(String message) {
+      getInstance();
+      instance.info(message);
+    }
+
+    @Override
+    public void info(String message, Object... args) {
+      getInstance();
+      instance.info(message, args);
+    }
+
+    @Override
+    public void info(String message, Throwable throwable) {
+      getInstance();
+      instance.info(message, throwable);
+    }
+
+    @Override
+    public void warn(String message) {
+      getInstance();
+      instance.warn(message);
+    }
+
+    @Override
+    public void warn(String message, Object... args) {
+      getInstance();
+      instance.warn(message, args);
+    }
+
+    @Override
+    public void warn(String message, Throwable throwable) {
+      getInstance();
+      instance.warn(message, throwable);
+    }
+
+    @Override
+    public void error(String message) {
+      getInstance();
+      instance.error(message);
+    }
+
+    @Override
+    public void error(String message, Object... args) {
+      getInstance();
+      instance.error(message, args);
+    }
+
+    @Override
+    public void error(String message, Throwable throwable) {
+      getInstance();
+      instance.error(message, throwable);
+    }
+
+    @Override
+    public void trace(String message) {
+      getInstance();
+      instance.trace(message);
+    }
+
+    @Override
+    public void trace(String message, Object... args) {
+      getInstance();
+      instance.trace(message, args);
+    }
+
+    @Override
+    public void trace(String message, Throwable throwable) {
+      getInstance();
+      instance.trace(message, throwable);
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+      getInstance();
+      return instance.isDebugEnabled();
+    }
+
+    @Override
+    public boolean isInfoEnabled() {
+      getInstance();
+      return instance.isInfoEnabled();
+    }
+
+    @Override
+    public boolean isWarnEnabled() {
+      getInstance();
+      return instance.isWarnEnabled();
+    }
+
+    @Override
+    public boolean isErrorEnabled() {
+      getInstance();
+      return instance.isErrorEnabled();
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+      getInstance();
+      return instance.isTraceEnabled();
+    }
+
+    @Override
+    public Optional<Level> getLoggerLevel() {
+      getInstance();
+      return instance.getLoggerLevel();
+    }
+
+    @Override
+    public void setLoggerLevel(Level level) {
+      getInstance();
+      instance.setLoggerLevel(level);
+    }
+
   }
 }
