a3bc1acb8a447276eeea4e1565a7a0702427ae0e
Guido Modarelli <guidomodarelli25@gmail.com>
2021-07-16 10:46:52 -0300

    [#454] broadcast client events

--- com.equo.node.packages/packages/equo-comm/src/comm-api.ts
+++ com.equo.node.packages/packages/equo-comm/src/comm-api.ts
@@ -1,30 +1,30 @@
 /****************************************************************************
-**
-** Copyright (C) 2021 Equo
-**
-** This file is part of Equo Framework.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Equo licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Equo. For licensing terms
-** and conditions see https://www.equoplatform.com/terms.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
+ **
+ ** Copyright (C) 2021 Equo
+ **
+ ** This file is part of Equo Framework.
+ **
+ ** Commercial License Usage
+ ** Licensees holding valid commercial Equo licenses may use this file in
+ ** accordance with the commercial license agreement provided with the
+ ** Software or, alternatively, in accordance with the terms contained in
+ ** a written agreement between you and Equo. For licensing terms
+ ** and conditions see https://www.equoplatform.com/terms.
+ **
+ ** GNU General Public License Usage
+ ** Alternatively, this file may be used under the terms of the GNU
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation. Please review the following
+ ** information to ensure the GNU General Public License requirements will
+ ** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+ **
+ ****************************************************************************/
 
-import { EquoService } from '@equo/service-manager'
+import { EquoService } from "@equo/service-manager";
 
+type Payload = any;
 export class EquoComm {
-
-    private userEventCallbacks: any = {};
+    private userEventCallbacks: Record<string, Function> = {};
     private ws?: WebSocket;
     private port: number;
 
@@ -34,11 +34,11 @@ export class EquoComm {
      */
     constructor(port: number) {
         this.port = port;
-        this.openComm()
+        this.openComm();
     }
 
     private openComm() {
-        if (this.ws !== undefined && this.ws.readyState !== WebSocket.CLOSED){
+        if (this.ws !== undefined && this.ws.readyState !== WebSocket.CLOSED) {
             return;
         }
         this.ws = new WebSocket(`ws://127.0.0.1:${this.port}`);
@@ -48,101 +48,99 @@ export class EquoComm {
             // For reasons I can't determine, onopen gets called twice
             // and the first time event.data is undefined.
             // Leave a comment if you know the answer.
-            if (event.data === undefined)
-                return;
-        }
+            if (event.data === undefined) return;
+        };
 
         this.ws.onmessage = (event: any): void => {
             this.receiveMessage(this.userEventCallbacks, event.data);
         };
 
-        this.ws.onclose = (event: any): void => {
-        };
+        this.ws.onclose = (): void => {};
     }
 
-    private receiveMessage(userEventCallbacks: any, event: any): void {
+    private receiveMessage(
+        userEventCallbacks: Record<string, Function>,
+        event: any,
+    ): void {
         if (event === undefined) {
             return;
         }
         try {
-            let parsedPayload = JSON.parse(event);
+            let parsedPayload: {
+                action: string;
+                params: Payload;
+            } = JSON.parse(event);
             let actionId = parsedPayload.action;
             if (actionId in userEventCallbacks) {
                 let params = parsedPayload.params;
-                if (parsedPayload.params) {
-                    userEventCallbacks[actionId](params);
-                } else {
-                    userEventCallbacks[actionId]();
-                }
+                userEventCallbacks[actionId](params);
             }
-        } catch (err) {
-
-        }
+        } catch (err) {}
     }
 
     // Expose the below methods via the equo interface while
-    // hiding the implementation of the method within the 
+    // hiding the implementation of the method within the
     // function() block
     private sendToCommServer(actionId: any, browserParams?: any): void {
         // Wait until the state of the comm is not ready and send the message when it is...
-        let receiveMessage = this.receiveMessage;
-        let userEventCallbacks = this.userEventCallbacks
         this.waitForCommConnection(this, () => {
             let event = JSON.stringify({
                 action: actionId,
-                params: browserParams
+                params: browserParams,
             });
             this.ws!.send(event);
-            receiveMessage(userEventCallbacks, event);
         });
-    };
+    }
 
     // Make the function wait until the connection is made...
     private waitForCommConnection(comm: EquoComm, callback: Function): void {
-        setTimeout(
-            () => {
-                if (this.ws !== undefined && this.ws!.readyState === WebSocket.OPEN) {
-                    if (callback != null) {
-                        callback();
-                    }
-                    return;
-                } else {
-                    try{
-                        this.openComm();
-                    }catch(err){}
-                    comm.waitForCommConnection(comm, callback);
+        setTimeout(() => {
+            if (
+                this.ws !== undefined &&
+                this.ws!.readyState === WebSocket.OPEN
+            ) {
+                if (callback != null) {
+                    callback();
                 }
-            }, 5); // wait 5 milisecond for the connection...
-    };
+                return;
+            } else {
+                try {
+                    this.openComm();
+                } catch (err) {}
+                comm.waitForCommConnection(comm, callback);
+            }
+        }, 5); // wait 5 milisecond for the connection...
+    }
     /**
      * Sends an action to execute in Framework.
      * @param {string} actionId
      * @param {JSON} [payload] - Optional
      * @returns {void}
      */
-    public send(actionId: any, payload?: any): void {
+    public send(actionId: string, payload?: Payload): void {
         this.sendToCommServer(actionId, payload);
-    };
+    }
     /**
      * Listens for an event with the given name.
      * @param {string} userEvent
      * @param {Function} callback
      * @returns {void}
      */
-    public on(userEvent: any, callback: Function) {
+    public on(userEvent: string, callback: Function): void {
         this.userEventCallbacks[userEvent] = callback;
-    };
-
+    }
 }
-/** 
+/**
  * @namespace
  * @description Comm API for usage within the Equo Framework.
- * 
+ *
  * This document specifies the usage methods for equo-comm.
  */
 export namespace EquoCommService {
-    const CommServiceId: string = 'equo-comm';
-    const queryParams: URLSearchParams = new URLSearchParams(window.location.search);
+    const CommServiceId: string = "equo-comm";
+    const queryParams: URLSearchParams = new URLSearchParams(
+        window.location.search,
+    );
     const portS: string | null = queryParams.get("equocommport");
     const port: number = portS === null ? 0 : +portS;
 
@@ -158,7 +156,7 @@ export namespace EquoCommService {
     export function create(): EquoService<EquoComm> {
         return {
             id: CommServiceId,
-            service: new EquoComm(port)
+            service: new EquoComm(port),
         };
     }
     /**
